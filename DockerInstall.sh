87c9d689bbe12fad8420141e5ab9bb7e9f99e710
Lewis <Lewis@MacBook-Pro.local>
2018-11-14 00:40:31 +0000

    first commit

---
+++ DockerInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function installdmg {
+    set -x
+    tempd=$(mktemp -d)
+    curl $1 > $tempd/pkg.dmg
+    listing=$(sudo hdiutil attach $tempd/pkg.dmg | grep Volumes)
+    volume=$(echo "$listing" | cut -f 3)
+    if [ -e "$volume"/*.app ]; then
+      sudo cp -rf "$volume"/*.app /Applications
+    elif [ -e "$volume"/*.pkg ]; then
+      package=$(ls $volume | grep pkg | head -1)
+      sudo installer -pkg "$volume"/"$package" -target /
+    fi
+    sudo hdiutil detach $volume
+    rm -rf $tempd
+    set +x
+}
+
+echo "Getting download link"
+DOWNLOAD_LINK="https://download.docker.com/mac/stable/Docker.dmg" 
+echo "Installing DMG"
+installdmg $DOWNLOAD_LINK
